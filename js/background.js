6cb4a5f44ea77cc2cb44342cefb655f3445fc2c5
Limjonghyun97 <jong99411@gmail.com>
2022-11-02 19:08:46 +0900

    ex1

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
-const images = ["0.jpg", "1.jpg", "2.jpg",]
+const images = ["0.jpg",]
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const body = document.querySelector("body");
 
-bgImage.src = `img/${chosenImage}`;
+body.style.background = `url(img/${chosenImage})`;
 
 document.body.appendChild(bgImage);
